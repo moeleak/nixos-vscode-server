a82b12aeadd9f67edf7ce7cd8858e75e241d8210
MoeLeak <i@leak.moe>
2026-06-02 12:32:47 +0800

    fix: restore nixpkgs flake input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "NixOS VSCode server";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
   outputs =
